03149d595afdeca934a43412a74eede8333b42e2
Ankit Kr Ghosh <66225746+ankitghosh00@users.noreply.github.com>
2020-06-04 17:17:36 +0530

    Create Chapter 1_Python quiz.py

---
+++ Chapter 1_Python quiz.py
@@ -0,0 +1,132 @@
+
+1.Question  1
+When Python is running in the interactive mode and displaying the chevron prompt (>>>) - what question is Python asking you?
+
+<A>  What would you like to do?
+<B>  What Python script would you like me to run?
+<C>  What is your favourite color?
+<D>  What is the next machine language instruction to run?
+
+AnS:  <B> What Python statement would you like me to run?
+
+
+
+
+1.Question  2
+What will the following program print out:
+>>> x = 15
+>>> x = x + 5
+>>> print(x)
+
+ <A>    20
+ <B>    5
+ <C>    15
+ <D>  "print x"
+ <E>   x + 5
+ 
+Ans:  <A>   20 
+ 
+ 
+ 
+
+3.Question 3
+Python scripts (files) have names that end with:
+
+<A>  .png
+<B>  .exe
+<C>  .doc
+<D>   .py
+
+
+Ans: <D> .py
+
+
+4.Question 4
+Which of these words are reserved words in Python ?
+
+
+<A>   if
+<B>   concat
+<C>   break
+<D>   todo
+<E>   machine
+
+
+AnS:  <A> if
+
+
+5.Question 5
+What is the proper way to say “good-bye” to Python?
+
+<A>   while
+<B>   #EXIT
+<C>   quit()
+<D>   // stop
+
+
+AnS: <C>  quit()
+
+
+
+
+6.Question 6
+Which of the parts of a computer actually executes the program instructions?
+
+<A>    Input/Output Devices
+<B>    Central Processing Unit
+<C>     Main Memory
+<D>    Secondary Memory
+
+
+Ans: <B>  Central Processing Unit
+
+
+
+7.Question 7
+What is "code" in the context of this course?
+
+<A>    A set of rules that govern the style of programs
+<B>    A password we use to unlock Python features
+<C>    A way to encrypt data during World War II
+<D>    A sequence of instructions in a programming language
+
+
+AnS: <D>   A sequence of instructions in a programming language
+
+
+
+8.Question 8
+A USB memory stick is an example of which of the following components of computer architecture?
+
+<A>   Secondary Memory
+<B>   Output Device
+<C>   Main Memory
+<D>   Central Processing Unit
+
+
+AnS: <A>  Secondary Memory
+
+
+9.Question 9
+What is the best way to think about a "Syntax Error" while programming?
+
+<A>   The computer has used GPS to find your location and hates everyone from your town
+<B>   The computer is overheating and just wants you to stop to let it cool down
+<C>   The computer needs to have its software upgraded
+<D>   The computer did not understand the statement that you entered
+
+
+AnS:<D>  The computer did not understand the statement that you entered
+
+
+
+
+10.Question 10
+Which of the following is not one of the programming patterns covered in Chapter 1?
+
+<A>  Repeated Steps
+<B>  Conditional Steps
+<C>  Sequential Steps
+<D>  Random steps
+
+AnS: <D> Random Steps
