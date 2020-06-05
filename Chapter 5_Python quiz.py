094cebdfa239a85dc4f2bbeeba920e4170464247
Ankit Kr Ghosh <66225746+ankitghosh00@users.noreply.github.com>
2020-06-05 14:08:51 +0530

    Create Chapter 5_Python quiz.py

---
+++ Chapter 5_Python quiz.py
@@ -0,0 +1,184 @@
+
+1.Question 1
+What is wrong with this Python loop:
+n = 5
+while n > 0 :
+    print(n)
+print('All done')
+
+<A>     This loop will run forever
+<B>      The print('All done') statement should be indented four spaces
+<C>      There should be no colon on the while statement
+<D>     while is not a Python reserved word
+
+
+
+AnS: <A>  This loop will run forever
+
+
+
+
+
+
+2.Question 2
+What does the break statement do?
+
+<A>      Exits the currently executing loop
+<B>      Exits the program
+<C>      Resets the iteration variable to its initial value
+<D>      Jumps to the "top" of the loop and starts the next iteration
+
+
+
+AnS:<A> Exits the currently executing loop
+
+
+
+
+
+
+
+3.Question 3
+What does the continue statement do?
+
+<A>      Jumps to the "top" of the loop and starts the next iteration
+<B>      Resets the iteration variable to its initial value
+<C>      Exits the program
+<D>      Exits the currently executing loop
+
+
+
+AnS:  <A>   Jumps to the "top" of the loop and starts the next iteration
+
+
+
+
+
+4.Question 4
+What does the following Python program print out?
+tot = 0 
+for i in [5, 4, 3, 2, 1] :
+    tot = tot + 1
+print(tot)
+
+<A>    10
+<B>    5
+<C>    0
+<D>    15
+
+
+
+AnS: <B>  5
+
+
+
+
+5.Question 5
+What is the iteration variable in the following Python code:
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends :
+     print('Happy New Year:',  friend)
+print('Done!')
+
+<A>    friend
+<B>    Sally
+<C>    Glenn
+<D>    Joseph
+
+
+
+AnS: <A> friend
+
+
+
+
+
+6.Question 6
+What is a good description of the following bit of Python code?
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends :
+     print('Happy New Year:',  friend)
+print('Done!')
+
+<A>     Count all of the elements in a list
+<B>     Find the smallest item in a list
+<C>     Sum all the elements of a list
+<D>     Compute the average of the elements in a list
+
+
+
+AnS:  <C>  Sum all the elements of a list
+
+
+
+
+7.Question 7
+What will the following code print out?
+smallest_so_far = -1
+for the_num in [9, 41, 12, 3, 74, 15] :
+   if the_num < smallest_so_far :
+      smallest_so_far = the_num
+print(smallest_so_far)
+Hint: This is a trick question and most would say this code has a bug - so read carefully
+
+<A>   -1
+<B>   74
+<C>   42
+<D>    3
+
+
+AnS: <A> -1
+
+
+
+
+8.Question 8
+What is a good statement to describe the is operator as used in the following if statement:
+if smallest is None :
+     smallest = value
+
+<A>     The if statement is a syntax error
+<B>     Looks up 'None' in the smallest variable if it is a string
+<C>      Is true if the smallest variable has a value of -1
+<D>     matches both type and value
+
+
+AnS: <B>   not Looks up 'None' in the smallest variable if it is a string orifThe if statement is a syntax error
+
+
+
+
+
+
+
+9.Question 9
+Which reserved word indicates the start of an "indefinite" loop in Python?
+
+<A>    break
+<B>    def
+<C>    for
+<D>    while
+<E>    indef
+
+
+AnS: <D> while 
+
+
+
+
+10.Question 10
+How many times will the body of the following loop be executed?
+n = 0
+while n > 0 :
+    print('Lather')
+    print('Rinse')
+print('Dry off!')
+
+<A>    This is an infinite loop
+<B>     5
+<C>     0
+<D>     1
+
+AnS: <A>  this is an indefinite loop
+
+
